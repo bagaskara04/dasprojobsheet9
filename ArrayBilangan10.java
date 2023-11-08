f42a9789379af5ea73a796ea43f497976eecdccc
karalion9 <sagabdespair01@gmail.com>
2023-11-08 15:18:42 +0700

    commit

---
+++ ArrayBilangan10.java
@@ -0,0 +1,24 @@
+/**
+ * ArrayBilangan
+ */
+public class ArrayBilangan10 {
+
+    public static void main(String[] args) {
+        // int[] bil = new int[4];
+        int[] bil = {5, 12867, -7, 2000000};
+
+        // bil[0] = 5;
+        // bil[1] = 12867;
+        // bil[2] = 7;
+        // bil[3] = 2000000;
+
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
