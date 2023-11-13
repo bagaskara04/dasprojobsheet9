3eba697d8bdf304108e157fd21feb84fbff42be5
karalion9 <sagabdespair01@gmail.com>
2023-11-13 19:12:21 +0700

    commit

--- ArrayRataNilai10.java
+++ ArrayRataNilai10.java
@@ -8,6 +8,7 @@ public class ArrayRataNilai10 {
 
         int[] nilaiMhs = new int[mahasiswa];
         double total = 0, total2 = 0, rata, rata2;
+        double lulus = 0, tidaklulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai Mahasiswa ke- " + (i + 1) + " : ");
@@ -17,14 +18,17 @@ public class ArrayRataNilai10 {
             if (nilaiMhs[i] > 70) {
                 //System.out.println("Mahasiswa ke- " + i + " lulus!");
                 total += nilaiMhs[i];
+                lulus++;
             } else {
                 //System.out.println("Mahasiswa ke- " + i + " tidak lulus!");
                 total2 += nilaiMhs[i];
+                tidaklulus++;
             }
         }
-        rata = total / nilaiMhs.length;
+
+        rata = total / lulus;
         System.out.print("Rata - rata nilai lulus = " + rata);
-        rata2 = total2 / nilaiMhs.length;
+        rata2 = total2 / tidaklulus;
         System.out.println();
         System.out.print("Rata - rata nilai tidak lulus = "+ rata2);
 
--- LinearSearch10.java
+++ LinearSearch10.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class LinearSearch10 {
     public static void main(String[] args) {
-        int[] arrayInt = {34,18,26,48,72,20,56,63};
-        int key = 20;
-        int hasil = 0;
+        Scanner input10 = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
+        int elemen = input10.nextInt();
 
+        int[] arrayInt = new int[elemen];
+        int key;
+        int hasil = 1;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            // if (arrayInt[i] == key) {
+            // hasil = i;
+            // break;
+            // }
+            System.out.print("Masukkan elemen array ke- " + i + " : ");
+            arrayInt[i] = input10.nextInt();
+            hasil = i;
+        }
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = input10.nextInt();
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.println("Key ada diposisi indeks ke- " + hasil);
                 break;
-            }
+            } else 
+                System.out.println("Key tidak ditemukan ");
+                break;
         }
-        System.out.println("key ada dalam array pada posisi indeks ke- "+hasil);
     }
 }
